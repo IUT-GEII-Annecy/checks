c946192a1f33b3369dbbf336c3832e6d656c598a
Geoffrey Vaquette <geoffrey.vaquette@univ-smb.fr>
2025-08-29 16:50:40 +0000

    fix rectangle test

--- tps/rectangle/__init__.py
+++ tps/rectangle/__init__.py
@@ -15,17 +15,18 @@ def compiles():
 
 
 @check50.check(compiles)
-def harry():
+def aire5x5():
     """Un rectangle de 5 par 5 a une aire de 25"""
-    check_debug(largeur=5, longueur=5, aire=25)
+    check_debug(largeur=5, longueur=5, aire="25.00")
 
 
 @check50.check(compiles)
-def dumbledore():
-    """Input of \"Dumbledore\" and \"Mould-on-the-Wold\" produces output \"Hello, Dumbledore, from Mould-on-the-Wold!\""""
-    check_debug(name="Dumbledore", place="Mould-on-the-Wold")
+def aire2x5():
+    """Un rectangle de 2 par 5 a une aire de 25"""
+    check_debug(largeur=2, longueur=5, aire="10.00")
 
 
 # Helpers
-def check_debug(largeur: float, longueur: float, aire: float):
-    check50.run("./debug").stdin(largeur).stdin(longueur).stdout(f"L'aire du rectangle est de {aire}!")
+def check_debug(largeur: float, longueur: float, aire: str):
+    actual = check50.run("./rectangle").stdin(largeur).stdin(longueur).stdout(f"L'aire du rectangle est de {aire}\")
+    
