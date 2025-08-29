f36e5521115640b68b1158804c24e8441e53df81
Geoffrey Vaquette <geoffrey.vaquette@univ-smb.fr>
2025-08-29 12:53:44 +0000

    Ajoute des prenoms dans le hello

--- hello/__init__.py
+++ hello/__init__.py
@@ -17,10 +17,10 @@ def compiles():
 @check50.check(compiles)
 def emma():
     """responds to name Emma"""
-    check50.run("./hello").stdin("Emma").stdout("Emma").exit()
+    check50.run("./hello").stdin("Emma").stdout("Bonjour, Emma").exit()
 
 
 @check50.check(compiles)
 def rodrigo():
     """responds to name Rodrigo"""
-    check50.run("./hello").stdin("Rodrigo").stdout("Rodrigo").exit()
+    check50.run("./hello").stdin("Rodrigo").stdout("Bonjour, Rodrigo").exit()
