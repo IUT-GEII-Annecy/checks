23af3a41f433c3f6d08ccd2320cf256c46649cf7
Geoffrey Vaquette <geoffrey.vaquette@univ-smb.fr>
2025-08-30 16:56:57 +0000

    Corrige tests communs

--- tps/geometrie/__init__.py
+++ tps/geometrie/__init__.py
@@ -1,4 +1,7 @@
-rectangle2 = __import__("check50").import_checks("../rectangle2")
-cercle2 = __import__("check50").import_checks("../cercle2")
+import check50
+
+check50.import_checks("../rectangle2")
+check50.import_checks("../cercle2")
+
 from rectangle2 import *
 from cercle2 import *
