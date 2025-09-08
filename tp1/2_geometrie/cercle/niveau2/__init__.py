768083839b2d5cee35f703734402ed65949eca8e
Geoffrey Vaquette <main.github.country044@passmail.net>
2025-09-08 18:08:51 +0200

    Update __init__.py

--- tp1/2_geometrie/cercle/niveau2/__init__.py
+++ tp1/2_geometrie/cercle/niveau2/__init__.py
@@ -1,6 +1,27 @@
 import check50
 import check50.c
+from pathlib import Path
+from contextlib import chdir, nullcontext  # <-- standard lib
 
+
+
+EXER_DIR = "2_geometrie/"
+MAIN = "cercle.c"
+
+
+def exercise_cwd():
+    """
+    Si hello.c est à la racine → pas de cd.
+    Si hello.c est dans 0_hello/ → cd 0_hello.
+    Sinon → erreur explicite.
+    """
+    if Path(MAIN).exists():
+        return nullcontext()
+    elif Path(EXER_DIR, MAIN).exists():
+        return chdir(EXER_DIR)   # <-- au lieu de check50.cd(...)
+    else:
+        raise check50.Failure(f"{MAIN} introuvable (./{MAIN} ou ./{EXER_DIR}/{MAIN}).")
+        
 check50.import_checks("../cercle/niveau1")
 from niveau1 import *
 
